6325a9e8df011040f41c1cc054678d855f101e45
Mayank8159 <mayankkgp8617@gmail.com>
2025-01-18 17:00:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("img1").addEventListener("click", function() {
+    this.classList.add("slide-left");
+});
+
+document.getElementById("img2").addEventListener("click", function() {
+    this.classList.add("slide-right");
+});
+
+document.getElementById("img3").addEventListener("click", function() {
+    this.classList.add("slide-left");
+});
+
+document.getElementById("img4").addEventListener("click", function() {
+    this.classList.add("slide-right");
+});
+
+document.getElementById("img5").addEventListener("click", function() {
+    this.classList.add("slide-left");
+});
